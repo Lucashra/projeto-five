f060f7638f08b5e0729c4fc0f934b6988bf61799
Lucas Alessandro <lucas_hra@hotmail.com>
2023-11-23 23:06:48 -0300

    Adicionando Jetstream e Livewire

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,45 @@
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <x-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-label for="email" value="{{ __('E-mail') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="password" value="{{ __('Senha') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4" style="display: flex;justify-content: space-between;">
+                <label for="remember_me" class="flex items-center">
+                    <x-checkbox id="remember_me" name="remember" />
+                    <span class="ms-2 text-sm text-gray-600">{{ __('Lembrar login') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-center mt-4">
+                <x-button class="ms-4">
+                    {{ __('Entrar') }}
+                </x-button>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Cadastrar</a>
+                @endif
+            </div>
+        </form>
+    </x-authentication-card>
+</x-guest-layout>
